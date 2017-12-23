39381630e1a61e15950276e9ce61c8541a00e0e5
Daniel Englisch <xeroaustria@hotmail.com>
2017-12-23 10:10:06 +0100

    Added mandelbrot sketch

--- HelloWorld/sketch.js
+++
@@ -1,8 +0,0 @@
-function setup() {
-  createCanvas(500,500);
-  stroke(255);
-}
-
-function draw() {
-  line(0,0,500,500);
-}
---
+++ Mandelbrot/sketch.js
@@ -0,0 +1,92 @@
+// Input settings
+var dx = -0.7453;
+var dy = 0.1127;
+var minR = - 6.5E-4;
+var maxIterations = 500;
+
+
+var maxR = -minR;
+  
+
+// Interface
+var iter;
+var button;
+
+function setup() {
+  createCanvas(500,500);
+  pixelDensity(1);
+  loadPixels();  
+
+  // Setup interface
+  iter = createInput('500');
+  iter.position(10, 510);
+  
+  button = createButton('Recalculate');
+  button.position(iter.x + iter.width+5, iter.y);
+  button.mousePressed(setValues);
+  calculate();
+
+}
+
+function setValues(){
+  if(maxIterations == iter.value())
+    return;
+  maxIterations = iter.value();
+  calculate();
+}
+
+function calculate() {
+  // For every pixel
+  for(var x = 0; x < width; x++){
+    for(var y = 0; y < height; y++){
+      // Memory mapping
+      var pix = (x+y*width)*4;
+
+      // Map pixel position between minR and maxR
+      var a = map(x,0,width,minR,maxR);
+      var b = map(y,0,height,minR,maxR);
+      a+=dx;
+      b+=dy;
+      var n = 0;
+      var initialA = a;
+      var initialB = b;
+
+      // Iterate
+      for(var n = 0; n < maxIterations;n++){
+        // Apply mandelbrot formula
+        var newA = a*a - b*b;
+
+        var newB = 2*a*b;
+
+        a = initialA + newA;
+        b = initialB + newB;
+
+        // If it gets towards infinity
+        if(abs(a + b) > 16)
+          break;
+      }
+
+      // Map iterations to brightness
+        //var brightness = map(n,0,100,0,255);
+        //var brightness = n*6 %255;
+      // Normalize
+      var brightness = map(n,0,maxIterations,0,1);
+      // Map to sqrt(brightness) function
+      brightness = map(sqrt(brightness),0,1,0,255);
+
+      // Ignore infinite values
+      if(n === maxIterations)
+          brightness = 0;
+       
+
+      // RGBA Values of pixel
+      pixels[pix+0] = brightness - 0.5*n % 255;
+      pixels[pix+1] = brightness + n % 255;
+      pixels[pix+2] = brightness * 1.2 % 255;
+      pixels[pix+3] = 255 - 0.5 * n   %255;
+      
+    }
+  }
+
+  updatePixels();
+}
